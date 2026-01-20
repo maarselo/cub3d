32e58c50b792a24460a6cec5c9388678c9099eb6
maarselo <marce@decaos.com>
2026-01-20 02:57:38 +0100

    feat:print Error\n first and the exactly error in red

--- src/error/handler_error.c
+++ src/error/handler_error.c
@@ -22,7 +22,9 @@ bool	ft_error_handler(t_error *error)
 {
 	if (!ft_has_error(error))
 		return (false);
+	ft_putendl_fd("Error\033[1;31m", 2);
 	ft_putendl_fd(error->message, 2);
+	ft_putstr_fd("\033[0m", 2);
 	if (error->is_freeable)
 		free(error->message);
 	free(error);
