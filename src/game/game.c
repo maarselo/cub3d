7fd87151c53e9ea721303656e393ce8df0a84f95
maarselo <marce@decaos.com>
2026-02-02 18:10:35 +0100

    feat: add game.c init

---
+++ src/game/game.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvillavi <mvillavi@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/29 19:15:12 by mvillavi          #+#    #+#             */
+/*   Updated: 2026/01/10 21:14:32 by mvillavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MLX42.h"
+#include "cub3d.h"
+#include "parser.h"
+
+#include <stdio.h>
+
+void	ft_game_loop(t_data *data)
+{
+	mlx_put_pixel(data->textures->north, 0, 0, data->colors->ceiling);
+	mlx_loop(data->mlx->window);
+}
