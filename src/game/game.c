d45bf631269d44fdee61936cb3f24cceedd20893
maarselo <marce@decaos.com>
2026-02-09 17:09:25 +0100

    feat: attach the wall images , and fixeg segfaults

--- src/game/game.c
+++ src/game/game.c
@@ -16,6 +16,18 @@
 #include "errorctx.h"
 #include <math.h>
 
+uint32_t	ft_get_pixel(mlx_image_t *texture, int col, int row)
+{
+	if ((uint32_t)col >= texture->width || (uint32_t)row >= texture->height || col < 0 || row < 0)
+		return (0);
+	uint32_t	pos = (row * texture->width + col) * 4;
+	uint8_t r = texture->pixels[pos];
+	uint8_t g = texture->pixels[pos + 1];
+	uint8_t b = texture->pixels[pos + 2];
+	uint8_t a = texture->pixels[pos + 3];
+	return (r << 24 | g << 16 | b << 8 | a);
+}
+
 void	ft_render(void *param)
 {
 	int		current_col; // current pixel column of the screen
@@ -36,105 +48,117 @@ void	ft_render(void *param)
 	int		wall_height; // how tall the wall looks on the screen
 	int		draw_start; // point where you start to draw the wall
 	int		draw_end; // point where you stop drawing the wall
-	//mlx_texture_t	*tex;
-	//double	wall_x; // in what x pixel of the wall the ray collides
-	uint32_t	color;
+	mlx_image_t	*wall_texture;
+	double	wall_x; // in what x pixel of the wall the ray collides
+	int		tex_x; // x pixel (col) of the image wall_x represents
+	double	step; // how many pixels to skip, to resize the image
+	double	tex_pos; // initial position of the texture to draw
 	t_data	*data;
 
 	data = (t_data *)param;
 	current_col = -1;
 	while (current_col++ < WINDOW_WIDTH)
 	{
-		camera_x = 2 * current_col / (double)WINDOW_WIDTH - 1;
-		ray_dir_x = data->player->dir_x + (data->player->plane_x * camera_x);
-		ray_dir_y = data->player->dir_y + (data->player->plane_y * camera_x);
+		camera_x = 2 * current_col / (double)WINDOW_WIDTH - 1; //calculas columna actual -1 - +1.(0-1920)
+		ray_dir_x = data->player->dir_x + (data->player->plane_x * camera_x); //te dicen hacia dónde estás mirando exactamente para esa columna de la pantalla.
+		ray_dir_y = data->player->dir_y + (data->player->plane_y * camera_x); //te dicen hacia dónde estás mirando exactamente para esa columna de la pantalla.
 		map_x = (int)data->player->pos_x;
 		map_y = (int)data->player->pos_y;
-		color = 0xFF6F3FF5;
 		if (ray_dir_x != 0)
-			delta_dist_x = fabs(1 / ray_dir_x);
-		else
+			delta_dist_x = fabs(1 / ray_dir_x); //Se calcula despues de la primera linea, cuanto es para la sig horizontal
+		else //solo en caso que el rayo sea del del medio
 			delta_dist_x = 1e30;
 		if (ray_dir_y != 0)
-			delta_dist_y = fabs(1 / ray_dir_y);
-		else
-			delta_dist_y = 1e30;
+			delta_dist_y = fabs(1 / ray_dir_y); //Se calcula despes de la primer alinea, cuanto es para la sig vertical
+		else //solo en caso para cero
+			delta_dist_y = 1e30; 
 		if (ray_dir_x < 0)
 		{
-			step_x = -1;
-			side_dist_x = (data->player->pos_x - map_x) * delta_dist_x;
+			step_x = -1; //Si va a la izquiera el rayo
+			side_dist_x = (data->player->pos_x - map_x) * delta_dist_x; //calcula distnacia  apriemr alinea horizontal
 		}
 		else
 		{
-			step_x = 1;
-			side_dist_x = (map_x + 1.0 - data->player->pos_x) * delta_dist_x;
+			step_x = 1;  //Si va a la derecha el rayo
+			side_dist_x = (map_x + 1.0 - data->player->pos_x) * delta_dist_x; //calcula distnacia  apriemr alinea horizontal
 		}
 		if (ray_dir_y < 0)
 		{
-			step_y = -1;
-			side_dist_y = (data->player->pos_y - map_y) * delta_dist_y;
+			step_y = -1; //Si va para arriba el rayo 
+			side_dist_y = (data->player->pos_y - map_y) * delta_dist_y; //calcula distancia a primera linea vertical
 		}
 		else
 		{
-			step_y = 1;
-			side_dist_y = (map_y + 1.0 - data->player->pos_y) * delta_dist_y;
+			step_y = 1; //Si va para abajo el rayo
+			side_dist_y = (map_y + 1.0 - data->player->pos_y) * delta_dist_y; //calcula distancia a primera linea vertical
 		}
 		hit = false;
 		while (!hit)
 		{
-			if (side_dist_x < side_dist_y)
+			if (side_dist_x < side_dist_y)  //Si es menor a distancia al primer rato verticla
 			{
-				side_dist_x += delta_dist_x;
-				map_x += step_x;
-				side = 0;
-			}
-			else
+				side_dist_x += delta_dist_x; //actualiamos valor a la siugiente linea vertica
+				map_x += step_x; //Usamos para seguir la direccion a la que apuntamos
+				side = 0;  //chocamos con una linea vertical
+			} 
+			else //Si es menor a distancia al primer rato horizontal
 			{
-				side_dist_y += delta_dist_y;
-				map_y += step_y;
-				side = 1;
+				side_dist_y += delta_dist_y; //actualizamos valor a ls isugiente linea horizontal
+				map_y += step_y; //Usamos para seguir la direccion a la que apuntamos
+				side = 1;  //chocamos con una line horizontal
 			}
-			if (data->map->map[map_y][map_x] == '1')
+			if (data->map->map[map_y][map_x] == '1') //comrpobamos que hemos llegado al final dela vista(muro)
 				hit = true;
 		}
 		if (side == 0)
 		{
-			perpendicular_dist = side_dist_x - delta_dist_x;
-			//wall_x = data->player->pos_y + ray_dir_y * perpendicular_dist;
+			perpendicular_dist = side_dist_x - delta_dist_x; //perpendicular_dist es realmente side_dist_x pero restandole el ultio incremento
+			wall_x = data->player->pos_y + ray_dir_y * perpendicular_dist;
 		}
 		else
 		{
-			perpendicular_dist = side_dist_y - delta_dist_y;
-			//wall_x = data->player->pos_x + ray_dir_x * perpendicular_dist;
-			color = 0xFF6038D1;
+			perpendicular_dist = side_dist_y - delta_dist_y; //perpendicular_dist es realmente side_dist_y pero restandole el ultio incremento
+			wall_x = data->player->pos_x + ray_dir_x * perpendicular_dist;
 		}
-		wall_height = (int)(WINDOW_HEIGHT / perpendicular_dist);
-		draw_start = (WINDOW_HEIGHT - wall_height) / 2;
-		draw_end = draw_start + wall_height;
-		if (draw_start < 0)
+		wall_x -= floor(wall_x);
+		wall_height = (int)(WINDOW_HEIGHT / perpendicular_dist); //altura  dee la pared a lo lejos o cerca
+		draw_start = (WINDOW_HEIGHT - wall_height) / 2; //donde se ha de iniciar el dibujo de la pared
+		draw_end = draw_start + wall_height; //donde parar el diujo de la pred
+		if (draw_start < 0) //proteger si en estas al lado dde la pared y que no pinte en -abajo de pantalla
 			draw_start = 0;
-		if (draw_end >= WINDOW_HEIGHT)
+		if (draw_end >= WINDOW_HEIGHT) //proteger que n estes al lado de la parex y pinta +arriba de pantalla
 			draw_end = WINDOW_HEIGHT - 1;
+		if (side == 0)
+		{
+			if (step_x == -1)
+				wall_texture = data->textures->east;
+			else 
+				wall_texture = data->textures->west;
+		}
+		else
+		{
+			if (step_y == -1)
+				wall_texture = data->textures->south;
+			else
+				wall_texture = data->textures->north;
+		}
+		tex_x = (int)((double)wall_texture->width * wall_x);
+		step = (double)wall_texture->height / wall_height;
+		tex_pos = (draw_start - WINDOW_HEIGHT / 2 + wall_height / 2) * step;
 		int	i = 0;
-		while (i < draw_start)
+		while (i < draw_start) //pintar suelo
 		{
 			mlx_put_pixel(data->mlx->img, current_col, i, data->colors->floor);
 			i++;
 		}
-		while (i < draw_end)
+		while (i < draw_end) //pintar pared
 		{
-			mlx_put_pixel(data->mlx->img, current_col, i, color);
+			int	pixel = ft_get_pixel(wall_texture, tex_x, (int)tex_pos);
+			mlx_put_pixel(data->mlx->img, current_col, i, pixel);
+			tex_pos += step;
 			i++;
 		}
-		/*if (side == 0 && data->player->dir_x == 1)
-			tex = data->textures->east;
-		else if (side == 0 && data->player->dir_x == -1)
-			tex = data->textures->west;
-		else if (side == 1 && data->player->dir_y == 1)
-			tex = data->textures->south;
-		else
-			tex = data->textures->north;*/
-		while (i < WINDOW_HEIGHT)
+		while (i < WINDOW_HEIGHT) //pintar cielo
 		{
 			mlx_put_pixel(data->mlx->img, current_col, i, data->colors->ceiling);
 			i++;
@@ -142,21 +166,23 @@ void	ft_render(void *param)
 	}
 }
 
+
+
 #include <stdlib.h>
 
+double	old_plane_x;
 void	ft_move(mlx_key_data_t key, void *param)
 {
 	t_data	*data;
 	double	speed;
 	double	rot_speed;
 	double	old_dir_x;
-	double	old_plane_x;
 	double	buffer;
 
 	data = (t_data *)param;
 	speed = 0.2;
 	rot_speed = 0.05;
-	buffer = 0.2;
+	buffer = 0.3;
 	if (key.key == MLX_KEY_ESCAPE)
 		exit(1);
 	if (key.key == MLX_KEY_W)
@@ -173,7 +199,7 @@ void	ft_move(mlx_key_data_t key, void *param)
 		if (data->map->map[(int)data->player->pos_y][(int)(data->player->pos_x - data->player->dir_x * (speed + buffer))] == '0')
 			data->player->pos_x -= data->player->dir_x * speed;
 	}
-	if (key.key == MLX_KEY_A)
+	if (key.key == MLX_KEY_A)	
 	{
 		if (data->map->map[(int)(data->player->pos_y - data->player->plane_y * (speed + buffer))][(int)data->player->pos_x] == '0')
 			data->player->pos_y -= data->player->plane_y * speed;
