7c7b36bebb53d903f00a3233c545c9faf68780f3
Francesc Banzo <12francescbs@gmail.com>
2026-02-10 19:47:34 +0100

    refactor: changed textures

--- src/game/game.c
+++ src/game/game.c
@@ -6,7 +6,7 @@
 /*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 19:15:12 by mvillavi          #+#    #+#             */
-/*   Updated: 2026/02/08 21:55:56 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2026/02/10 19:46:48 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,22 +166,18 @@ void	ft_render(void *param)
 	}
 }
 
-
-
 #include <stdlib.h>
 
 void	ft_move(mlx_key_data_t key, void *param)
 {
 	t_data	*data;
 	double	speed;
-	double	rot_speed;
 	double	old_dir_x;
 	double	old_plane_x;
 	double	buffer;
 
 	data = (t_data *)param;
 	speed = 0.2;
-	rot_speed = 0.05;
 	buffer = 0.3;
 	if (key.key == MLX_KEY_ESCAPE)
 		exit(1);
