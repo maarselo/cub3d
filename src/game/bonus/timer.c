ae1cca6632cd37a7730bf8ccf8d7500a0ee3d7a3
maarselo <marce@decaos.com>
2026-02-25 13:59:56 +0100

    fix: imrpove the rendiment of the fft_timer to not print the same time 60 times per second

--- src/game/bonus/timer.c
+++ src/game/bonus/timer.c
@@ -6,7 +6,7 @@
 /*   By: mvillavi <mvillavi@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 19:14:28 by mvillavi          #+#    #+#             */
-/*   Updated: 2026/02/25 13:11:21 by mvillavi         ###   ########.fr       */
+/*   Updated: 2026/02/25 13:57:30 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,24 @@
 
 void	ft_timer(void *param)
 {
-	char	*tmp;
-	char	*timer_str;
-	t_data	*data;
+	int			seconds;
+	char		*str[2];
+	t_data		*data;
+	static int	old_time;
+	static bool	not_first_time;
 
 	data = (t_data *)param;
+	seconds = (int)mlx_get_time();
+	if (not_first_time == false)
+		not_first_time = true;
+	else if (old_time ==  seconds)
+		return ;
 	if (data->textures->timer)
 		mlx_delete_image(data->mlx->window, data->textures->timer);
-	tmp = ft_itoa((int)mlx_get_time());
-	timer_str = ft_strjoin("Time played: ", tmp);
-	free(tmp);
-	data->textures->timer = mlx_put_string(data->mlx->window, timer_str, 0, 0);
-	free(timer_str);
+ 	str[0] = ft_itoa(seconds);
+	str[1] = ft_strjoin("Time played: ", str[0]);
+	data->textures->timer = mlx_put_string(data->mlx->window, str[1], 0, 0);
+	free(str[0]);
+	free(str[1]);
+	old_time = seconds;
 }
